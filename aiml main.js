c9b4857e902e310ef221f6395d715e3ffd5e293b
MORUMPALLI VINAY KUMAR <120386613+vinay7330@users.noreply.github.com>
2023-11-08 20:53:50 +0530

    Add files via upload

---
+++ aiml main.js
@@ -0,0 +1,104 @@
+
+
+const apiKey = "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";
+
+const maxImage = 10;
+
+const selectedImageNumber = undefined;
+
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (min - max + 1)) + min;
+}
+
+
+function disableGenerateButton() {
+    document.getElementById("generate").disabled = true;
+}
+
+
+function enableGenerateButton() {
+    document.getElementById("generate").disabled = false;
+}
+
+
+function clearImageGrid() {
+    const imageGrid = document.getElementById("image-grid");
+    imageGrid.innerHTML = "";
+}
+
+
+async function generateImages(input) {
+    disableGenerateButton();
+    clearImageGrid();
+
+    const loading = document.getElementById("loading");
+    loading.style.display = "block";
+
+    const imageUrls = [];
+
+    for (let i = 0; i < maxImage; i++) {
+    
+        const randomNumber = getRandomNumber(1, 10000);
+        const prompt = `${input} ${randomNumber}`;
+
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/prompthero/openjourney", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({ inputs: prompt }),
+            }
+        );
+
+        if (!response.ok) {
+            const error = document.createElement("h2");
+            error.style.color = "red";
+            error.style.fontSize = "1.2rem";
+            const result = document.getElementById("result");
+            error.innerHTML = response.text();
+            result.appendChild(error);
+        }
+        const blob = await response.blob();
+        const imgUrl = URL.createObjectURL(blob);
+
+        imageUrls.push(imgUrl);
+
+        const img = document.createElement("img");
+        img.src = imgUrl;
+        img.alt = `art-${i + 1}`;
+        img.onclick = () => downloadImage(imgUrl, i);
+        document.getElementById("image-grid").appendChild(img);
+    }
+    loading.style.display = "none";
+    enableGenerateButton();
+    selectedImageNumber = null;
+}
+
+document.getElementById("generate").addEventListener("click", () => {
+    const input = document.getElementById("user-prompt").value;
+    console.log(input);
+    generateImages(input);
+});
+
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("user-prompt").value;
+    console.log(input);
+    generateImages(input);
+});
+
+
+function downloadImage(imgUrl, imageNumber) {
+    const link = document.createElement("a");
+    link.href = imgUrl;
+    link.download = `image-${imageNumber + 1}.jpg`;
+    link.click();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("splash").classList.add("loaded");
+});
